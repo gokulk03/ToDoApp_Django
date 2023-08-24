3fbd51f4d4a0850b49b3c85c5dac63ce1742a182
Gokul <gokulsrinivask2020@gmail.com>
2023-08-24 18:33:05 +0530

    first_commit

---
+++ todo_main/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from todo_app.models import Task
+
+def home(request):
+    tasks = Task.objects.filter(is_completed=False).order_by('-created_at')
+    context = {'tasks':tasks}
+    return render(request,'home.html',context)
